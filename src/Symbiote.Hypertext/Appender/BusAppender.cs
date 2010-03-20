5ee43c7acd5ac51ba9a0d140a66530dbd278f8ab
Alex Robson <asrobson@gmail.com>
2010-03-20 02:28:15 -0500

    Modifications to log4net bus appender

--- src/Symbiote.Hypertext/Appender/BusAppender.cs
+++ src/Symbiote.Hypertext/Appender/BusAppender.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using log4net.Appender;
 using log4net.Core;
-using log4net.Layout;
 using StructureMap;
-using Symbiote.Log4Net;
 using Symbiote.Telepathy.Appender;
 
 namespace Symbiote.Jackalope.Appender
@@ -30,27 +28,6 @@ namespace Symbiote.Jackalope.Appender
         public BusAppender()
         {
             _bus = ObjectFactory.GetInstance<IBus>();
-        }
-    }
-
-    public class BusConfigurator : AppenderConfigurator<BusAppender,BusConfigurator>
-    {
-        protected override void Initialize()
-        {
-            _appender.ActivateOptions();
-            _appender.Name = "busAppender";
-            _appender.ExchangeName = "log4Net";
-            _appender.Threshold = Level.All;
-            var layout =
-                new PatternLayout(
-                    Pattern
-                        .New()
-                        .Message()
-                        .Newline()
-                        .ToString()
-                    );
-            layout.ActivateOptions();
-            _appender.Layout = layout;
         }
     }
 }
---
+++ src/Symbiote.Hypertext/Appender/BusConfigurator.cs
@@ -0,0 +1,33 @@
+﻿using log4net.Core;
+using log4net.Layout;
+using Symbiote.Log4Net;
+
+namespace Symbiote.Jackalope.Appender
+{
+    public class BusConfigurator : AppenderConfigurator<BusAppender,BusConfigurator>
+    {
+        public BusConfigurator ExchangeName(string exchangeName)
+        {
+            _appender.ExchangeName = exchangeName;
+            return this;
+        }
+
+        protected override void Initialize()
+        {
+            _appender.ActivateOptions();
+            _appender.Name = "busAppender";
+            _appender.ExchangeName = "log4Net";
+            _appender.Threshold = Level.All;
+            var layout =
+                new PatternLayout(
+                    Pattern
+                        .New()
+                        .Message()
+                        .Newline()
+                        .ToString()
+                    );
+            layout.ActivateOptions();
+            _appender.Layout = layout;
+        }
+    }
+}
