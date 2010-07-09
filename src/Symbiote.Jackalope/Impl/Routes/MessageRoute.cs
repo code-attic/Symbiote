2dd7d098b1120f15bb865d898087ec44ca23a14d
Alex Robson <asrobson@gmail.com>
2010-07-09 07:18:22 -0500

    Beginning work on route definitions

---
+++ src/Symbiote.Jackalope/Impl/Routes/MessageRoute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Symbiote.Jackalope.Impl.Routes
+{
+    public interface IDefineRoute
+    {
+        IDefineRoute For<TMessage>();
+        IDefineRoute SendTo(string exchangeName);
+        IDefineRoute WithSubject<TMessage>(Func<TMessage, string> subjectBuilder);
+        IDefineRoute WithSubject(string subject);
+    }
+
+    public class RouteBuilder : IDefineRoute
+    {
+        public IDefineRoute For<TMessage>()
+        {
+
+            return this;
+        }
+
+        public IDefineRoute SendTo(string exchangeName)
+        {
+
+            return this;
+        }
+
+        public IDefineRoute WithSubject<TMessage>(Func<TMessage, string> subjectBuilder)
+        {
+
+            return this;
+        }
+
+        public IDefineRoute WithSubject(string subject)
+        {
+
+            return this;
+        }
+    }
+
+    public interface IRouteMessage<TMessage>
+    {
+        string ExchangeName { get; set; }
+        Func<TMessage, string> SubjectBuilder { get; set; }
+    }
+
+    public class MessageRoute<TMessage> : 
+        IRouteMessage<TMessage>
+    {
+        public string ExchangeName { get; set; }
+        public Func<TMessage, string> SubjectBuilder { get; set; }
+    }
+}
