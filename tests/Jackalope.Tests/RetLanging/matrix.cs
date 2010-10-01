3d14cc5dea51f0408ee0f9444ce26726627b324b
Alex Robson <asrobson@gmail.com>
2010-09-30 20:38:38 -0400

    Checking in missing files

---
+++ tests/Jackalope.Tests/RetLanging/matrix.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Retlang.Channels;
+using Retlang.Fibers;
+
+namespace Jackalope.Tests.RetLanging
+{
+    public class ChannelBus<TMessage>
+    {
+        protected IChannel<TMessage> Channel { get; set; }
+        protected ConcurrentQueue<IFiber> Fibers { get; set; }
+
+
+    }
+
+    public class Matrix<TKey, TMessage>
+    {
+        public ConcurrentDictionary<TKey, ChannelBus<TMessage>> Channels { get; set; }
+        
+    }
+}
---
+++ tests/Jackalope.Tests/RetLanging/when_blasting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Machine.Specifications;
+using Retlang.Channels;
+using Retlang.Fibers;
+
+namespace Jackalope.Tests.RetLanging
+{
+    public class when_blasting
+    {
+        protected static int receiveCount = 0;
+        protected static int messageCount = 1000000;
+        protected static int fiberz = 10;
+        private Because of = () =>
+                                 {
+                                     var queues = new List<IFiber>();
+                                     var reset = new AutoResetEvent(false);
+                                     var channel = new QueueChannel<int>();
+
+
+                                     var updateLock = new object();
+
+                                     Action<int> onReceive = delegate
+                                     {
+                                         Thread.Sleep(3);
+                                         //lock (updateLock)
+                                         //{
+                                             receiveCount++;
+                                             if (receiveCount == messageCount)
+                                             {
+                                                 reset.Set();
+                                             }
+                                         //}
+                                     };
+                                     for (var i = 0; i < fiberz; i++)
+                                     {
+                                         var fiber = new PoolFiber();
+                                         fiber.Start();
+                                         queues.Add(fiber);
+                                         channel.Subscribe(fiber, onReceive);
+                                     }
+                                     for (var i = 0; i < messageCount; i++)
+                                     {
+                                         channel.Publish(i);
+                                     }
+                                     reset.WaitOne(1000, false);
+                                     queues.ForEach(delegate(IFiber q) { q.Dispose(); });
+                                 };
+
+        private It should_process_a_million_smessages = () => receiveCount.ShouldEqual(messageCount);
+    }
+}
