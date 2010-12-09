26c87ed146b0b209e1c1e6f614fe153454670fa6
Alex Robson <asrobson@gmail.com>
2010-12-09 09:52:28 -0500

    Rearranging namespaces in Symbiote.Actor

---
+++ src/Symbiote.Actor/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Symbiote.Actor
+{
+    public abstract class ActorEvent
+        : IEvent
+    {
+        public Guid EventId { get; set; }
+        public string ActorType { get; set; }
+        public string ActorId { get; set; }
+        public DateTime UtcTimeStamp { get; set; }
+    }
+}
---
+++ tests/Actor.Tests/Domain/Events/NewAddress.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Actor.Tests.Domain.Model;
+using Symbiote.Actor;
+
+namespace Actor.Tests.Domain.Events
+{
+    public class DriverAddedAddress
+        : ActorEvent
+    {
+        public int StreetNumber { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public DriverAddedAddress() {}
+
+        public DriverAddedAddress( int streetNumber, string streetName, string city, string state, string zip )
+        {
+            StreetNumber = streetNumber;
+            StreetName = streetName;
+            City = city;
+            State = state;
+            Zip = zip;
+        }
+    }
+
+    public class DriverChangedName
+        : ActorEvent
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
