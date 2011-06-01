8473da92153755e6cdbf8d96abf45889b257fbb1
Jim Cowart <ashbylane@gmail.com>
2011-06-01 10:53:05 -0400

    Merging localChannelUpdate with master branch, and cleaning local repo

--- misc/experimental/Symbiote.Http/OwinBody.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Symbiote.Http
-{
-    public delegate Action OwinBody(
-        Func<ArraySegment<byte>, Action, bool> onNext,
-        Action<Exception> onError,
-        Action onComplete );
-}
--- misc/experimental/Symbiote.Http/OwinResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Symbiote.Http
-{
-    public delegate void OwinResponse(
-        string status,
-        IDictionary<string, string> headers,
-        OwinBody body);
-}
